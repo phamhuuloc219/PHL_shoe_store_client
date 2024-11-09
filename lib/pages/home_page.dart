c136f5b11c77437d4ab6462ae6b118a159a9c3f8
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2024-11-09 23:33:23 +0700

    feat: fetch data from firebase and show product in home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:phl_shoe_store_client/controller/home_controller.dart';
 import 'package:phl_shoe_store_client/pages/login_page.dart';
 import 'package:phl_shoe_store_client/pages/product_description.dart';
 import 'package:phl_shoe_store_client/widgets/drop_down_btn.dart';
@@ -12,78 +13,80 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('PHL Shoe Stores'),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () {
-                GetStorage box = GetStorage();
-                box.erase();
-                Get.offAll(LoginPage());
-              }),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: Chip(label: Text('Category')),
-                );
-              },
-            ),
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: DropDownBtn(
-                  items: ["RS: Low to high", "Rs: High to low"],
-                  onSelected: (selected) {},
-                  selectedItemText: "Sort",
-                ),
-              ),
-              Flexible(
-                  child: MultiSelectDropDown(
-                onSelectionChanged: (selectedItems) {},
-                items: ["Rs: Low to high", "Rs: High to low"],
-              )),
-            ],
-          ),
-          Expanded(
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.8,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return ProductCard(
-                    name: "Nike Pegasus",
-                    imageUrl:
-                        "https://media.gettyimages.com/id/506922838/photo/nike-pegasus-design-shoes-and-logo.jpg?s=612x612&w=0&k=20&c=By0UzuxrGvTiQ4sW4OhFBxxbPfbnida2jkH-yNwXqkk=",
-                    price: 200,
-                    offerTag: "30% off",
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ProductDescriptionPage(),
-                          ));
-                    },
-                  );
+    return GetBuilder<HomeController>(builder: (ctrl) {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('PHL Shoe Stores'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () {
+                  GetStorage box = GetStorage();
+                  box.erase();
+                  Get.offAll(LoginPage());
                 }),
-          )
-        ],
-      ),
-    );
+          ],
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Chip(label: Text('Category')),
+                  );
+                },
+              ),
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: DropDownBtn(
+                    items: ["RS: Low to high", "Rs: High to low"],
+                    onSelected: (selected) {},
+                    selectedItemText: "Sort",
+                  ),
+                ),
+                Flexible(
+                    child: MultiSelectDropDown(
+                  onSelectionChanged: (selectedItems) {},
+                  items: ["Rs: Low to high", "Rs: High to low"],
+                )),
+              ],
+            ),
+            Expanded(
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.8,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8),
+                  itemCount: ctrl.products.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(
+                      name: ctrl.products[index].name ?? "",
+                      imageUrl: ctrl.products[index].image ??
+                          "https://media.gettyimages.com/id/506922838/photo/nike-pegasus-design-shoes-and-logo.jpg?s=612x612&w=0&k=20&c=By0UzuxrGvTiQ4sW4OhFBxxbPfbnida2jkH-yNwXqkk=",
+                      price: ctrl.products[index].price ?? 0,
+                      offerTag: "30% off",
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ProductDescriptionPage(),
+                            ));
+                      },
+                    );
+                  }),
+            )
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/pages/product_description.dart
+++ lib/pages/product_description.dart
@@ -1,73 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:phl_shoe_store_client/controller/home_controller.dart';
 
 class ProductDescriptionPage extends StatelessWidget {
   const ProductDescriptionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Product Details',
-          style: TextStyle(fontWeight: FontWeight.bold),
+    return GetBuilder<HomeController>(builder: (ctrl) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Product Details',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                'https://media.gettyimages.com/id/506922838/photo/nike-pegasus-design-shoes-and-logo.jpg?s=612x612&w=0&k=20&c=By0UzuxrGvTiQ4sW4OhFBxxbPfbnida2jkH-yNwXqkk=',
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Nike Pegasus',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Product description",
-              style: TextStyle(fontSize: 16, height: 1.5),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Rs: 300",
-              style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              maxLines: 3,
-              decoration: InputDecoration(
-                labelText: 'Enter your Billing Address',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  "https://firebasestorage.googleapis.com/v0/b/huuloc-firebase.appspot.com/o/phl_shoe_stores%2Fpuma%2Fz6002304171344_51ef4c5e8765068d4e0a2aceba746343.jpg?alt=media&token=c564e414-72fe-43ff-86a9-a5cc679caada",
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Buy Now",
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+              SizedBox(height: 20),
+              Text(
+                'Nike Pegasus',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Product description",
+                style: TextStyle(fontSize: 16, height: 1.5),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Rs: 300",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                maxLines: 3,
+                decoration: InputDecoration(
+                  labelText: 'Enter your Billing Address',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      backgroundColor: Colors.blue),
-                )),
-          ],
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Buy Now",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: Colors.blue),
+                  )),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
